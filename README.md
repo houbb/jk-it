9d9e7c3c18e1e03d50454b1f5f67b47a2ef445ba
binbin.hou <binbin.hou@huifu.com>
2025-03-31 17:00:11 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+jike blogs
+
+如何启动：
+
+```
+npm install
+```
+
+```
+npm run docs:dev
+```
+
+## 验证文档编译
+
+```
+npm run docs:build
+```
---
+++ src/.vuepress/navbar/zh.ts
@@ -0,0 +1,10 @@
+import { navbar } from "vuepress-theme-hope";
+
+//https://icon-sets.iconify.design/
+export const zhNavbar = navbar([
+  {
+    text: "全部文章",
+    icon: "pen-to-square",
+    link: "/posts/",
+  },
+]);
